617e43efdcad253c4e4cb86a7541fce60875dd69
山本 了大 <RyotaYamamoto@yamamotoryoudai-no-MacBook-Air.local>
2015-09-06 16:59:11 +0900

    処理細分化

--- app/Controller/IndicesController.php
+++ app/Controller/IndicesController.php
@@ -2,13 +2,18 @@
 class IndicesController extends AppController {
 	private $config;
 
-	public $view_only = true;
+	private $view_only = true;
 	private $cookie_auth_user;
+
+	private $date_info = array();
+	private $holiday_array = array();
+	private $season_info = array();
 
 	public function beforeFilter()
 	{
 		if(!isset($config)) $this->config = Configure::read();
 	}
+
 	public function index()
 	{
 		$this->view_only = false;
@@ -22,159 +27,46 @@ class IndicesController extends AppController {
 			$this->auth();
 		}
 
-		$this_day = ($this->request->query('date') && strtotime($this->request->query('date')) > strtotime('2010-01-01'))? $this->request->query('date'):date('Y-m-d');
-		$this_day_strtotime = strtotime($this_day);
-		$real_today = date('Y-n-j');
-
-		$today_y = date('Y',$this_day_strtotime);
-		$today_m = date('m',$this_day_strtotime);
-		$today_d = date('d',$this_day_strtotime);
-		$today_n = date('n',$this_day_strtotime);
-		$today_j = date('j',$this_day_strtotime);
-
-		$before_y = date('Y',strtotime($today_y.'-'.$today_m.'-01 -1 month'));
-		$before_m = date('m',strtotime($today_y.'-'.$today_m.'-01 -1 month'));
-		$before_d = date('d',strtotime($today_y.'-'.$today_m.'-01 -1 month'));
-		$before_n = date('n',strtotime($today_y.'-'.$today_m.'-01 -1 month'));
-		$before_j = date('j',strtotime($today_y.'-'.$today_m.'-01 -1 month'));
-
-		$after_y = date('Y',strtotime($today_y.'-'.$today_m.'-01 +1 month'));
-		$after_m = date('m',strtotime($today_y.'-'.$today_m.'-01 +1 month'));
-		$after_d = date('d',strtotime($today_y.'-'.$today_m.'-01 +1 month'));
-		$after_n = date('n',strtotime($today_y.'-'.$today_m.'-01 +1 month'));
-		$after_j = date('j',strtotime($today_y.'-'.$today_m.'-01 +1 month'));
-
-		$before_l = date('d',strtotime($today_y.'-'.$today_m.'-01 -1 day'));
-		$today_l = date('d',strtotime($after_y.'-'.$after_m.'-01 -1 day'));
-		$before_lj = date('j',strtotime($today_y.'-'.$today_m.'-01 -1 day'));
-		$today_lj = date('j',strtotime($after_y.'-'.$after_m.'-01 -1 day'));
-		$before_lw = date('w',strtotime($today_y.'-'.$today_m.'-01 -1 day'));
-		$today_lw = date('w',strtotime($after_y.'-'.$after_m.'-01 -1 day'));
-
-		$today_month_first_weekend = date('w',strtotime($today_y.'-'.$today_m.'-01'));
-		$today_month_first_weekend_str = $this->config['WEEKEND'][$today_month_first_weekend];
-
-		$calendar_before_month_days = array();
-		for($i = 0;$i<$today_month_first_weekend;$i++)
-		{
-		    $calendar_before_month_days[$i] = $before_lj-($today_month_first_weekend-1-$i);
-		}
-
-		$calendar_after_month_days = array();
-		for($i = $today_lw;$i<6;$i++)
-		{
-		    $calendar_after_month_days[$today_lw+(count($calendar_after_month_days)+1)] = count($calendar_after_month_days)+1;
-		}
+		$this->makeDateData();
+		$this->makeDaysDaysOfBeforeAndAfter();
+		$this->makeHolidayArray();
+		$this->getSeason();
 
 		$calendar_main = array();
 		$calendar_row_count = 0;
 		$calendaer_last_date = '';
-		foreach($calendar_before_month_days as $key => $val)
+		foreach($this->date_info['calendar_before_month_days'] as $key => $val)
 		{
 		    $calendar_main[$calendar_row_count][$key]['type'] = 'before';
 		    $calendar_main[$calendar_row_count][$key]['day'] = $val;
-		    $calendar_main[$calendar_row_count][$key]['day_full'] = $before_y.'-'.$before_n.'-'.$val;
+		    $calendar_main[$calendar_row_count][$key]['day_full'] = $this->date_info['before_y'].'-'.$this->date_info['before_n'].'-'.$val;
 		}
-		for($i=1;$i<=$today_lj;$i++)
+		for($i=1;$i<=$this->date_info['today_lj'];$i++)
 		{
-		    $calendar_main[$calendar_row_count][date('w',strtotime($today_y.'-'.$today_m.'-'.$i))]['type'] = 'main';
-		    $calendar_main[$calendar_row_count][date('w',strtotime($today_y.'-'.$today_m.'-'.$i))]['day'] = $i;
-		    $calendar_main[$calendar_row_count][date('w',strtotime($today_y.'-'.$today_n.'-'.$i))]['day_full'] = $today_y.'-'.$today_n.'-'.$i;
-		    $calendaer_last_date = $today_y.'-'.$today_m.'-'.$i;
-		    if(date('w',strtotime($today_y.'-'.$today_m.'-'.$i)) == 6) $calendar_row_count++;
+		    $calendar_main[$calendar_row_count][date('w',strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-'.$i))]['type'] = 'main';
+		    $calendar_main[$calendar_row_count][date('w',strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-'.$i))]['day'] = $i;
+		    $calendar_main[$calendar_row_count][date('w',strtotime($this->date_info['today_y'].'-'.$this->date_info['today_n'].'-'.$i))]['day_full'] = $this->date_info['today_y'].'-'.$this->date_info['today_n'].'-'.$i;
+		    $calendaer_last_date = $this->date_info['today_y'].'-'.$this->date_info['today_m'].'-'.$i;
+		    if(date('w',strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-'.$i)) == 6) $calendar_row_count++;
 		}
-		foreach($calendar_after_month_days as $key => $val)
+		foreach($this->date_info['calendar_after_month_days'] as $key => $val)
 		{
 		    $calendar_main[$calendar_row_count][$key]['type'] = 'after';
 		    $calendar_main[$calendar_row_count][$key]['day'] = $val;
-		    $calendar_main[$calendar_row_count][$key]['day_full'] = $after_y.'-'.$after_n.'-'.$val;
-		    $calendaer_last_date = $after_y.'-'.$after_n.'-'.$val;
-		}
-
-		$holiday_array = array();
-		$filenames = array();
-		$filenames['today'] = WWW_ROOT.'files/json/'.$today_y.$today_m.'.js';
-		$filenames['before'] = WWW_ROOT.'files/json/'.$before_y.$before_m.'.js';
-		$filenames['after'] = WWW_ROOT.'files/json/'.$after_y.$after_m.'.js';
-
-		$jsons = array();
-		$handles = array();
-		foreach($filenames as $when => $filename)
-		{
-		    $jsons[$when] = json_decode('['.file_get_contents($filename,true).']',true);
-		}
-
-		foreach ($jsons as $json_ret)
-		{
-		    foreach($json_ret as $json_val)
-		    {
-		        if(!strlen($json_val['jHoliday'])) continue;
-		        if($json_val['jHoliday'] == '振替') $json_val['jHoliday'] .= '休日';
-		        $holiday_array[$json_val['jYear'].'-'.$json_val['jMonth'].'-'.$json_val['jDay']] = $json_val['jHoliday'];
-		    }
-		}
-		unset($jsons);
-
-		$season = '';
-		$season_no = '';
-		if(strtotime('2015-05-01') <= strtotime($real_today))
-		{
-			$season = 'season';
-			$season .= ' '.$this->config['SEASON'][$today_n];
-			$season_no = ($today_j%3 == 0)? '03':'';
-			if(!strlen($season_no))
-			{
-				$season_no = ($today_j%2 == 0)? '02':'01';
-			}
+		    $calendar_main[$calendar_row_count][$key]['day_full'] = $this->date_info['after_y'].'-'.$this->date_info['after_n'].'-'.$val;
+		    $calendaer_last_date = $this->date_info['after_y'].'-'.$this->date_info['after_n'].'-'.$val;
 		}
 
 		$db_data = $this->getDbData($calendar_main,$calendaer_last_date);
 
+		$this->setDateInfo();
 		$this->set(array(
 			'view_only' => $this->view_only,
-			'db_data' => $db_data,
-			'this_day' => $this_day,
-			'this_day_strtotime' => $this_day_strtotime,
-			'real_today' => $real_today,
-
-			'today_y' => $today_y,
-			'today_m' => $today_m,
-			'today_d' => $today_d,
-			'today_n' => $today_n,
-			'today_j' => $today_j,
-
-			'before_y' => $before_y,
-			'before_m' => $before_m,
-			'before_d' => $before_d,
-			'before_n' => $before_n,
-			'before_j' => $before_j,
-
-			'after_y' => $after_y,
-			'after_m' => $after_m,
-			'after_d' => $after_d,
-			'after_n' => $after_n,
-			'after_j' => $after_j,
-
-			'before_l' => $before_l,
-			'today_l' => $today_l,
-			'before_lj' => $before_lj,
-			'today_lj' => $today_lj,
-			'before_lw' => $before_lw,
-			'today_lw' => $today_lw,
-
-			'today_month_first_weekend' => $today_month_first_weekend,
-			'today_month_first_weekend_str' => $today_month_first_weekend_str,
-
-			'calendar_before_month_days' => $calendar_before_month_days,
-
-			'calendar_after_month_days' => $calendar_after_month_days,
-
 			'calendar_main' => $calendar_main,
-
-			'holiday_array' => $holiday_array,
-
-			'season' => $season,
-			'season_no' => $season_no,
+			'holiday_array' => $this->holiday_array,
+			'season' => $this->season_info['name'],
+			'season_no' => $this->season_info['no'],
+			'db_data' => $db_data,
 		));
 		return;
 	}
@@ -259,24 +151,123 @@ class IndicesController extends AppController {
 			setcookie('r_m_33_u',$_COOKIE['r_m_33_u'],strtotime('today + 6 month'),'/');
 		}
 		$this->cookie_auth_user = (isset($_COOKIE['r_m_33_u']))? $_COOKIE['r_m_33_u']:'mao';
+		return;
+	}
+
+	private function makeDateData()
+	{
+		$this->date_info['this_day'] = ($this->request->query('date') && strtotime($this->request->query('date')) > strtotime('2010-01-01'))? $this->request->query('date'):date('Y-m-d');
+		$this->date_info['this_day_strtotime'] = strtotime($this->date_info['this_day']);
+		$this->date_info['real_today'] = date('Y-n-j');
+
+		$this->date_info['today_y'] = date('Y',$this->date_info['this_day_strtotime']);
+		$this->date_info['today_m'] = date('m',$this->date_info['this_day_strtotime']);
+		$this->date_info['today_d'] = date('d',$this->date_info['this_day_strtotime']);
+		$this->date_info['today_n'] = date('n',$this->date_info['this_day_strtotime']);
+		$this->date_info['today_j'] = date('j',$this->date_info['this_day_strtotime']);
+
+		$minus_1_month = strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-01 -1 month');
+		$this->date_info['before_y'] = date('Y',$minus_1_month);
+		$this->date_info['before_m'] = date('m',$minus_1_month);
+		$this->date_info['before_d'] = date('d',$minus_1_month);
+		$this->date_info['before_n'] = date('n',$minus_1_month);
+		$this->date_info['before_j'] = date('j',$minus_1_month);
+
+		$plus_1_month = strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-01 +1 month');
+		$this->date_info['after_y'] = date('Y',$plus_1_month);
+		$this->date_info['after_m'] = date('m',$plus_1_month);
+		$this->date_info['after_d'] = date('d',$plus_1_month);
+		$this->date_info['after_n'] = date('n',$plus_1_month);
+		$this->date_info['after_j'] = date('j',$plus_1_month);
+
+		$minus_1_day = strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-01 -1 day');
+		$last_day = strtotime($this->date_info['after_y'].'-'.$this->date_info['after_m'].'-01 -1 day');
+		$this->date_info['before_l'] = date('d',$minus_1_day);
+		$this->date_info['today_l'] = date('d',$last_day);
+		$this->date_info['before_lj'] = date('j',$minus_1_day);
+		$this->date_info['today_lj'] = date('j',$last_day);
+		$this->date_info['before_lw'] = date('w',$minus_1_day);
+		$this->date_info['today_lw'] = date('w',$last_day);
+
+		$this->date_info['today_month_first_weekend'] = date('w',strtotime($this->date_info['today_y'].'-'.$this->date_info['today_m'].'-01'));
+		$this->date_info['today_month_first_weekend_str'] = $this->config['WEEKEND'][$this->date_info['today_month_first_weekend']];
+		return;
+	}
+
+	private function makeDaysDaysOfBeforeAndAfter()
+	{
+		$this->date_info['calendar_before_month_days'] = array();
+		for($i = 0;$i<$this->date_info['today_month_first_weekend'];$i++)
+		{
+		    $this->date_info['calendar_before_month_days'][$i] = $this->date_info['before_lj']-($this->date_info['today_month_first_weekend']-1-$i);
+		}
+
+		$this->date_info['calendar_after_month_days'] = array();
+		for($i = $this->date_info['today_lw'];$i<6;$i++)
+		{
+		    $this->date_info['calendar_after_month_days'][$this->date_info['today_lw']+(count($this->date_info['calendar_after_month_days'])+1)] = count($this->date_info['calendar_after_month_days'])+1;
+		}
+		return;
+	}
+
+	private function makeHolidayArray()
+	{
+		$filenames = array();
+		$filenames['today'] = WWW_ROOT.'files/json/'.$this->date_info['today_y'].$this->date_info['today_m'].'.js';
+		$filenames['before'] = WWW_ROOT.'files/json/'.$this->date_info['before_y'].$this->date_info['before_m'].'.js';
+		$filenames['after'] = WWW_ROOT.'files/json/'.$this->date_info['after_y'].$this->date_info['after_m'].'.js';
+
+		$jsons = array();
+		$handles = array();
+		foreach($filenames as $when => $filename)
+		{
+		    $jsons[$when] = json_decode('['.file_get_contents($filename,true).']',true);
+		}
+
+		foreach ($jsons as $json_ret)
+		{
+		    foreach($json_ret as $json_val)
+		    {
+		        if(!strlen($json_val['jHoliday'])) continue;
+		        if($json_val['jHoliday'] == '振替') $json_val['jHoliday'] .= '休日';
+		        $this->holiday_array[$json_val['jYear'].'-'.$json_val['jMonth'].'-'.$json_val['jDay']] = $json_val['jHoliday'];
+		    }
+		}
+		unset($jsons);
+		return;
+	}
+
+	private function getSeason()
+	{
+		$this->season_info['name'] = 'season';
+		$this->season_info['no'] = '';
+		$this->season_info['name'] .= ' '.$this->config['SEASON'][$this->date_info['today_n']];
+		$this->season_info['no'] = ($this->date_info['today_j']%3 == 0)? '03':'';
+		if(!strlen($this->season_info['no']))
+		{
+			$this->season_info['no'] = ($this->info['today_j']%2 == 0)? '02':'01';
+		}
+		return;
+	}
+
+	private function setDateInfo()
+	{
+		foreach($this->date_info as $key => $val)
+		{
+			$this->set($key,$val);
+		}
+		return;
 	}
 
 	private function getDbData($calendar_main,$calendaer_last_date)
 	{
+		$this->loadModel('Index');
 		$user = (isset($this->cookie_auth_user))? $this->cookie_auth_user:'guest';
 		if($this->view_only) $user = 'mao';
 		if($user == 'ryota') $user = 'mao';
-		$mongo = new MongoClient();
-		$db = $mongo->selectDB('calendar');
-		$shift_collection = $db->shift;
-		$ret = $shift_collection->find(array('status'=>0,'user'=>$user,'date'=>array('$gte'=>new MongoDate(strtotime($calendar_main[0][0]['day_full'].' 00:00:00 +0900')),'$lte'=> new MongoDate(strtotime($calendaer_last_date.' 00:00:00 +0900')))));
 		$db_data = array();
-		foreach($ret as $r)
-		{
-			$db_date = (array) $r['date'];
-			$db_data[date('Y-m-j',$db_date['sec'])] = $r['shift'];
-			$db_data[date('Y-n-j',$db_date['sec'])] = $r['shift'];
-		}
+		$db_data = $this->Index->getMongoData($user,array('$gte'=>new MongoDate(strtotime($calendar_main[0][0]['day_full'].' 00:00:00 +0900')),'$lte'=> new MongoDate(strtotime($calendaer_last_date.' 00:00:00 +0900'))));
+
 		return $db_data;
 	}
 }
